ed180e39706d70a437154c4ea7889cef0e24e710
Muhammad Dede Alfaruq <dede.alfaruqq@gmail.com>
2025-01-28 13:45:11 +0700

    feat : adding navbar (#4)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import Navbar from "./components/navbar/Navbar";
 
 const poppins = Poppins({
   variable: "--font-poppins",
@@ -21,7 +22,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${poppins.variable} antialiased`}>{children}</body>
+      <body className={`${poppins.variable} antialiased`}>
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
