285ff8f3895a01dee800903c3d642f19e22e7435
Inggis Kurnia Trisiawan <inggiskurnia32@gmail.com>
2025-02-14 09:27:31 +0700

    chore: change next.config

--- next.config.ts
+++ next.config.ts
@@ -1,24 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
-    domains: [
-      "store.storeimages.cdn-apple.com",
-      "lsco.scene7.com",
-      "www.lego.com",
-      "images.samsung.com",
-      "i5.walmartimages.com",
-      "images.tokopedia.net",
-      "images-na.ssl-images-amazon.com",
-      "static.nike.com",
-      "encrypted-tbn2.gstatic.com",
-      "encrypted-tbn3.gstatic.com",
-      "www.mixandgrind.com.au",
-      "encrypted-tbn1.gstatic.com",
-      "encrypted-tbn0.gstatic.com",
-      "m.media-amazon.com",
-      "product-images.weber.com",
+    remotePatterns: [
+      { protocol: "https", hostname: "store.storeimages.cdn-apple.com" },
+      { protocol: "https", hostname: "lsco.scene7.com" },
+      { protocol: "https", hostname: "www.lego.com" },
+      { protocol: "https", hostname: "images.samsung.com" },
+      { protocol: "https", hostname: "i5.walmartimages.com" },
+      { protocol: "https", hostname: "images.tokopedia.net" },
+      { protocol: "https", hostname: "images-na.ssl-images-amazon.com" },
+      { protocol: "https", hostname: "static.nike.com" },
+      { protocol: "https", hostname: "encrypted-tbn2.gstatic.com" },
+      { protocol: "https", hostname: "encrypted-tbn3.gstatic.com" },
+      { protocol: "https", hostname: "www.mixandgrind.com.au" },
+      { protocol: "https", hostname: "encrypted-tbn1.gstatic.com" },
+      { protocol: "https", hostname: "encrypted-tbn0.gstatic.com" },
+      { protocol: "https", hostname: "m.media-amazon.com" },
+      { protocol: "https", hostname: "product-images.weber.com" },
     ],
   },
 };
