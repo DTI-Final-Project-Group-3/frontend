bafd7fe6dc43eb13188c8dcddcc2cb1b20e8118c
mdedealf <dede.alfaruqq@gmail.com>
2025-02-16 16:30:30 +0700

    feat : adding header and order management page for admin

--- src/app/(admin)/admin/order-management/page.tsx
+++ src/app/(admin)/admin/order-management/page.tsx
@@ -2,13 +2,16 @@ import React from "react";
 
 const OrderManagementPage = () => {
   return (
-    <main>
-      This is OrderManagementPage
-      <div>Order 1</div>
-      <div>Order 2</div>
-      <div>Order 3</div>
-      <div>Order 4</div>
-    </main>
+    <section className="w-full rounded-2xl bg-white p-7 min-h-[calc(100vh-178px)]">
+      {/* Title */}
+      <div className="flex flex-wrap items-center justify-between gap-2">
+        <h2 className="text-xl font-semibold">Order Management</h2>
+      </div>
+      {/* Content */}
+      <div className="mt-7 w-full overflow-hidden">
+        <p>Table</p>
+      </div>
+    </section>
   );
 };
 
