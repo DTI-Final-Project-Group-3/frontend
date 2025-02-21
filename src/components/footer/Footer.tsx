de9b48e13dae7df04aed5f0a8042d608fb0b999d
mdedealf <dede.alfaruqq@gmail.com>
2025-02-21 18:55:20 +0700

    chore : refactoring some style and ui

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,10 +2,10 @@ import React, { FC } from "react";
 
 const Footer: FC = () => {
   return (
-    <footer className="w-full bg-black md:mt-16 mt-5">
+    <footer className="w-full bg-black py-8">
       <div className="flex flex-col gap-6 items-center md:max-w-4xl lg:max-w-[1340px] mx-auto w-full py-12 text-white font-light">
         <div className="flex items-center gap-6 text mt-4 justify-between w-full">
-          <span>&#169; 2025 Warehub. All rights reserved</span>
+          <span className="font-normal">&#169; 2025 Warehub. All rights reserved</span>
           <div className="hidden lg:flex gap-4">
             <span>Terms</span>
             <span>Privacy</span>
