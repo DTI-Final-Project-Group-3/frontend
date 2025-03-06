88dd0734ce1787a96781e20644bbe1fd7e852562
Hilman Mauludin <32255027+hilmatrix@users.noreply.github.com>
2025-03-06 23:29:24 +0700

    refactor account and warehouse management code and optimizing warehouse assignment fetch code

--- src/app/(admin)/admin/account-management/page.tsx
+++ src/app/(admin)/admin/account-management/page.tsx
@@ -1,11 +1,12 @@
 "use client"
+import { AdminCard } from "@/components/account-management/AdminCard";
+import { toast } from "@/hooks/use-toast";
 import { UserAdminDetail } from "@/types/models/userAdminDetail";
 import { useSession } from "next-auth/react";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 
 const admin_url = `${process.env.NEXT_PUBLIC_BACKEND_URL}${process.env.NEXT_PUBLIC_ADMIN}`;
-const placeholderImage = "https://dummyimage.com/150x150/cccccc/ffffff&text=No+Image";
 
 const AccountManagementPage = () => {
   const { data: session, status } = useSession();
@@ -22,11 +23,11 @@ const AccountManagementPage = () => {
           if (data.success) {
             setAdmins(data.data);
           } else {
-            alert("Failed to fetch user details");
+            toast({title: "Failed", description: "Failed to fetch user details", duration: 2000,});
           }
         } catch (error) {
           console.error("Error fetching admin warehouses:", error);
-          alert("Error fetching admin warehouses");
+          toast({title: "Error", description: "Error fetching admin warehouses", duration: 2000,});
         }
       };
       fetchAdminWarehouse();
@@ -57,21 +58,6 @@ const AccountManagementPage = () => {
   );
 };
 
-const AdminCard = ({ admin }: { admin: UserAdminDetail }) => {
-  return (
-    <div className="bg-gray-100 p-5 rounded-xl shadow-md flex flex-col items-center">
-      <img
-        src={admin.profileImageUrl?.trim() ? admin.profileImageUrl : placeholderImage}
-        alt={`${admin.fullname}'s profile`}
-        className="w-24 h-24 rounded-full object-cover mb-3"
-      />
-      <h3 className="text-lg font-semibold">{admin.fullname}</h3>
-      <p className="text-sm text-gray-600">{admin.email}</p>
-      <p className="text-sm font-medium mt-2">Admin ID: {admin.id}</p>
-      <p className="text-sm font-medium mt-2">Warehouse ID: {admin.warehouseId}</p>
-      <p className="text-sm font-medium mt-2">Assigner ID: {admin.userAssignerId}</p>
-    </div>
-  );
-};
+
 
 export default AccountManagementPage;
